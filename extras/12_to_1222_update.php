7f800a3040cf7f1389eeac9772dc496a0ba63012
Koos <pampoen10@yahoo.com>
2013-03-20 18:27:49 +0200

    Check whether magic_quotes_runtime is active before attempting to turn magic_quotes off in extern.php, install.php and the database update script.

--- extras/12_to_1222_update.php
+++ extras/12_to_1222_update.php
@@ -46,7 +46,8 @@ define('PUN_DEBUG', 1);
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
 // Turn off magic_quotes_runtime
-set_magic_quotes_runtime(0);
+if (get_magic_quotes_runtime())	
+	set_magic_quotes_runtime(0);
 
 // Turn off PHP time limit
 @set_time_limit(0);
